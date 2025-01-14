d84b3a51c0dbcd9fdaaf27a1c6058bafc4060e81
kalderlunt <kalderlunt@gmail.com>
2025-01-14 12:18:43 +0100

    Init Project

---
+++ RtsProject/Source/RtsProject/RtsProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RtsProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARtsProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARtsProjectGameMode();
+};
+
+
+
