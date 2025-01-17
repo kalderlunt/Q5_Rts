3adb0aa9877bc7ac4974e4f831603902335b5a77
kalderlunt <kalderlunt@gmail.com>
2025-01-17 12:52:15 +0100

    Delete Project

--- RtsProject/Source/RtsProject/RtsProject.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RtsProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RtsProject, "RtsProject" );
-
-DEFINE_LOG_CATEGORY(LogRtsProject)
- 
